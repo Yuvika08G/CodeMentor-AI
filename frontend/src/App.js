a693a9a514a8a3d4bc13d8c00c4dc2092a5f092e
Yuvika08G <yuvikagupta0608@gmail.com>
2026-06-04 20:51:29 +0530

    Fixed Toggle theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -151,16 +151,11 @@ const runCode = async () => {
   );
 }
 
-function Card({ title, content }) {
+function Card({ title, content, isDark }) {
   return (
     <div className="bg-[#0b1220] border border-gray-700 p-5 rounded-xl mb-4">
-      <h3 className="text-xl font-bold mb-3">
-        {title}
-      </h3>
-
-      <p>
-        {content}
-      </p>
+      <h3 className="text-xl font-bold mb-3 text-white">{title}</h3>
+      <p className={isDark ? "text-gray-300" : "text-gray-800"}>{content}</p>
     </div>
   );
 }
